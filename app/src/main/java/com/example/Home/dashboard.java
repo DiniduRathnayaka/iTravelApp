613452b55056b7d8c42b49e41cc727f3415a5041
Btec-19825 <68671484+Btec-19825@users.noreply.github.com>
2020-10-04 04:37:45 +0530

    Profile upgrade complete

--- app/src/main/java/com/example/Home/dashboard.java
+++ app/src/main/java/com/example/Home/dashboard.java
@@ -1,22 +1,28 @@
 package com.example.Home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.itravel.R;
 import com.example.profile_upgrade.Viewpaymentlogs;
 import com.example.profile_upgrade.profile_upgrade_main;
-import com.example.profile_upgrade.profile_upgrade_main_pre_pay_method;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener;
 
-public class dashboard extends AppCompatActivity {
+public class dashboard extends AppCompatActivity implements OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
@@ -35,6 +41,7 @@ public class dashboard extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
@@ -52,9 +59,74 @@ public class dashboard extends AppCompatActivity {
         });
         button2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                Intent i=new Intent(dashboard.this, Viewpaymentlogs.class);
-                startActivity(i);
+                Intent j=new Intent(dashboard.this, Viewpaymentlogs.class);
+                startActivity(j);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_home ){
+            Toast.makeText(this, "Loading Home", Toast.LENGTH_SHORT).show();
+            Intent k = new Intent(dashboard.this, dashboard.class);
+            startActivity(k);
+        }
+        else if (item.getItemId() == R.id.menu_locations ){
+            Toast.makeText(this, "Loading Locations", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menu_hotels ){
+            Toast.makeText(this, "Loading Hotels", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menu_favourites ){
+            Toast.makeText(this, "Loading Favourites", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menu_aboutus ){
+            Toast.makeText(this, "Loading About Us", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menu_contactus ){
+            Toast.makeText(this, "Loading Contact Us", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menu_upgrade ){
+            Toast.makeText(this, "Loading Upgrade", Toast.LENGTH_SHORT).show();
+            Intent l=new Intent(dashboard.this, profile_upgrade_main.class);
+            startActivity(l);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/example/profile_upgrade/Intro_Page.java
+++ app/src/main/java/com/example/profile_upgrade/Intro_Page.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.Home.dashboard;
+import com.example.itravel.R;
 
 public class Intro_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        Intent i=new Intent(Intro_Page.this, dashboard.class);
-        startActivity(i);
+        setContentView(R.layout.activity_intro__page);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Intro_Page.this, dashboard.class);
+                startActivity(i);
+            }
+        }, 3000);
+
     }
 }
